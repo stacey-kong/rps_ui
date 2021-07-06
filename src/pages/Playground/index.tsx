d1a685a43af8f711829222c200680b0c83b57015
stacey <727@stlnd.net>
2021-07-06 17:59:40 +0800

    add create game popup

--- src/pages/Playground/index.tsx
+++ src/pages/Playground/index.tsx
@@ -1,34 +1,78 @@
 import { useEffect, useState } from "react";
-import { ethers } from "ethers";
-// import "./index.scss";
+import FormControl from "@material-ui/core/FormControl";
+import InputLabel from "@material-ui/core/InputLabel";
+import InputAdornment from "@material-ui/core/InputAdornment";
+import Input from "@material-ui/core/Input";
+import { ReactComponent as Eth } from "../../assets/svg/eth.svg";
+import { ReactComponent as Rock } from "../../assets/svg/rock.svg";
+import { ReactComponent as Paper } from "../../assets/svg/paper.svg";
+import { ReactComponent as Scissors } from "../../assets/svg/scissors.svg";
+
+import "./index.scss";
 
 interface PlaygroundProps {
-  account: string;
+  account: string | null;
+  balance: number | null;
 }
 
 export default function Playground(props: PlaygroundProps) {
-  //   const [account, setAccount] = useState<string | null>(null);
+  const [popup, setPopup] = useState<boolean>(false);
+  const [amount, setAmount] = useState<string>("");
 
-  //   async function requestAccount() {
-  //     let accounts = await window.ethereum.request({
-  //       method: "eth_requestAccounts",
-  //     });
-  //     return accounts;
-  //   }
+  const handleAmountChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setAmount(event.target.value);
+  };
 
-  //   const loadBlockchainData = async function name() {
-  //     let accounts = await requestAccount();
-  //     setAccount(accounts[0]);
-  //     console.log(accounts);
-  //   };
-
-  // useEffect(() => {
-  //   loadBlockchainData();
-  // });
   return (
-    <div className="container">
-      <h1>Playground</h1>
-      <p>Your account: {props.account}</p>
-    </div>
+    <>
+      <div className="container">
+        <div className="userInfo">
+          <h1>Playground</h1>
+          <div className="account">
+            <p>
+              Account:{" "}
+              {`${props.account?.slice(0, 5)}...${props.account?.slice(-4)}`}
+            </p>
+            <p>Balance: {props.balance?.toFixed(2)} ETH</p>
+          </div>
+        </div>
+        <div className="creatbtn">
+          <span
+            onClick={() => {
+              setPopup((prev) => !prev);
+            }}
+          >
+            Create Your Game
+          </span>
+        </div>
+      </div>
+      {popup && (
+        <div className="popup">
+          <div className="amount">
+            <FormControl fullWidth>
+              <InputLabel htmlFor="standard-adornment-amount">
+                Amount
+              </InputLabel>
+              <Input
+                id="standard-adornment-amount"
+                value={amount}
+                onChange={handleAmountChange}
+                startAdornment={
+                  <InputAdornment position="start">
+                    <Eth width="20" />
+                  </InputAdornment>
+                }
+              />
+            </FormControl>
+          </div>
+
+          <div className="choices">
+            <Rock className="choice" id="rock" viewBox="0 0 200 450" fill="pink" stroke="rgb(131, 213, 228, 0.863)" strokeWidth="10" stroke-linecap="round"/>
+            <Paper className="choice" id="paper" viewBox="50 20 400 50" fill="pink" stroke="rgb(131, 213, 228, 0.863)" strokeWidth="10" stroke-linecap="round"/>
+            <Scissors className="choice" id="scissors" viewBox="250 80 400 200" fill="pink" stroke="rgb(131, 213, 228, 0.863)" strokeWidth="10" stroke-linecap="round"/>
+          </div>
+        </div>
+      )}
+    </>
   );
 }
