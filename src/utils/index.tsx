561c7139e7f81b9b1e43d7495d7c918047e24929
stacey <727@stlnd.net>
2021-07-14 17:46:05 +0800

    add utils

--- src/utils/index.tsx
+++ src/utils/index.tsx
@@ -1,7 +1,7 @@
 import { ethers } from "ethers";
 
-export const convertToBignumber = function (number:number) {
-  return ethers.BigNumber.from(number)
+export const convertToBignumber = function (number: number) {
+  return ethers.BigNumber.from(number);
 };
 
 export const formatNumber = function (bignumber: ethers.BigNumber) {
@@ -45,4 +45,14 @@ export const formatDate = function (time: ethers.BigNumber) {
   /*alert(str);*/
 
   return str;
+};
+
+export const formatResponse = async function (res: any) {
+
+  //wait 2 blocks for transaction confirmation
+  let receipt = await res.wait(1);
+  // console.log(`receipt:${JSON.stringify(receipt)}`)
+  let sumEvent = receipt.events.pop();
+  let sum = sumEvent.args[0];
+  return sum;
 };
