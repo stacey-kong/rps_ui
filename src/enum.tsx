4a1a9cb4f67f0b36fb52392bc6301ea3084af1ea
stacey <727@stlnd.net>
2021-07-15 14:26:47 +0800

    add setting for production deployment

--- src/enum.tsx
+++ src/enum.tsx
@@ -19,4 +19,12 @@ export enum RESULT {
   DEALERWIN = 201,
 }
 
-export const contractAddress = "0xa513E6E4b8f2a923D98304ec87F64353C4D5C853";
+const verifyContractAddress = () => {
+  if (process.env.NODE_ENV === "production") {
+    return "0xbef444230B8005F255C859E7eea90AD58da74684";
+  } else {
+    return "0xa513E6E4b8f2a923D98304ec87F64353C4D5C853";
+  }
+};
+
+export const contractAddress = verifyContractAddress();
