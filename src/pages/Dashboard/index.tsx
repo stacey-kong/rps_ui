ae5e457c8d465b45c710e65198808d26de939ae7
stacey <727@stlnd.net>
2021-07-21 11:45:03 +0800

    adjust button style

--- src/pages/Dashboard/index.tsx
+++ src/pages/Dashboard/index.tsx
@@ -12,7 +12,9 @@ export default function Dashboard(props: DashboardProps) {
       <div className="container">
         <h1>Welcome to the ROCK PAPER SCISSORS Game </h1>
         <h3>Click to connect your Metamask account!</h3>
-        <span onClick={props.onClick}>Connect!</span>
+        <span onClick={props.onClick} className="button">
+          Connect!
+        </span>
       </div>
     </>
   );
